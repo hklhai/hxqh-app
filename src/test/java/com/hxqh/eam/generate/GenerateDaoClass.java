a7ce46f017fd1da0e4c38fa50991d6f4069e0bde
hklhai <hkhai@outlook.com>
2017-06-08 17:50:15 +0800

    db table analyze

--- src/test/java/com/hxqh/eam/generate/GenerateDaoClass.java
+++ src/test/java/com/hxqh/eam/generate/GenerateDaoClass.java
@@ -1,5 +1,7 @@
 package com.hxqh.eam.generate;
 
+import com.hxqh.eam.common.FileUtil;
+
 import java.io.*;
 
 /**
@@ -18,54 +20,20 @@ public class GenerateDaoClass {
 
 
 		for (String string : MBOName) {
-			String mboStr = txt2String(mboFile).replaceAll("XXXXX", string+"Dao").replaceAll("YYYYY",string);
-			WriteStringToFile("src/"+string+"Dao"+".java",mboStr);
+			String mboStr = FileUtil.txt2String(mboFile).replaceAll("XXXXX", string+"Dao").replaceAll("YYYYY",string);
+			FileUtil.WriteStringToFile("src/"+string+"Dao"+".java",mboStr);
 
 
 			//daoImpl
 			StringBuilder sb = new StringBuilder(string);
 			sb.setCharAt(0, Character.toLowerCase(sb.charAt(0)));
-			String daoImplStr = txt2String(daoImpl).replaceAll("XXXX", string+"Dao").replaceAll("YYYY",string)
+			String daoImplStr = FileUtil.txt2String(daoImpl).replaceAll("XXXX", string+"Dao").replaceAll("YYYY",string)
 					.replaceAll("ZZZZ",string+"DaoImpl").replaceAll("WWWW",sb.toString()+ "Dao");
-			WriteStringToFile("src/"+string+"DaoImpl"+".java",daoImplStr);
+			FileUtil.WriteStringToFile("src/"+string+"DaoImpl"+".java",daoImplStr);
 		}
 		
 		System.out.println("Generate Success!");
 	}
 
-	/*
-	 * 往文件里写入字符串
-	 */
-	public static void WriteStringToFile(String filePath,String context) {
-		try {
-			File file = new File(filePath);
-			PrintStream ps = new PrintStream(new FileOutputStream(file));
-			ps.println(context);// 往文件里写入字符串
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * 读取txt文件的内容
-	 * 
-	 * @param file
-	 *            想要读取的文件对象
-	 * @return 返回文件内容
-	 */
-	public static String txt2String(File file) {
-		StringBuilder result = new StringBuilder();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));// 构造一个BufferedReader类来读取文件
-			String s = null;
-			while ((s = br.readLine()) != null) {// 使用readLine方法，一次读一行
-				result.append(System.lineSeparator() + s);
-			}
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result.toString();
-	}
 
 }
---
+++ src/test/java/com/hxqh/eam/test/TableFormat.java
@@ -0,0 +1,27 @@
+package com.hxqh.eam.test;
+
+import com.hxqh.eam.common.FileUtil;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Created by Ocean Lin on 2017/6/8.
+ */
+public class TableFormat {
+
+    @Test
+    public void test() {
+        File file = new File("src/test/resources/0410.log");
+        String s = FileUtil.txt2String(file);
+        //System.out.println(s);
+        String[] split = s.split("\r\n");
+        for (int i = 1; i < split.length; i++) {
+            String[] rows = split[i].split("导出了");
+            //System.out.println(rows[0]);
+            //System.out.println(rows[1]);
+        }
+
+    }
+
+}
