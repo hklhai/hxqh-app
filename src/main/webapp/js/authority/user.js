9fcab5724cf3e8782e5fd8f85b9a232df6a519f0
aym19900825 <825185228@qq.com>
2017-08-18 11:24:05 +0800

    authority

--- src/main/webapp/js/authority/user.js
+++ src/main/webapp/js/authority/user.js
@@ -6,24 +6,35 @@ $(function () {
         el: "#customer-data",
         data: {
             userList:[],
-            user:[]
+            userName:'',
+            roleName:'',
+            selected:'',
+            roleList:[],
+            pwd:'000000',
+            email:'',
+            isNew: ture
         },
         methods:{
             add:function(){
+                this.initBox();
                 $(".box").show();
             },
             edit:function(item){
                 var self = this;
+                self.isNew = false;
+                self.pwd = "000000";
                 $(".box").show();
                 $.ajax({
-                    url: _ctx+"/system/userDetail",
+                    url: _ctx+"/system/userDetailData",
                     method: "get",
                     dataType: "json",
                     data:{
                         id: item.id
                     },
                     success: function (data) {
-                        self.user = data;
+                        self.userName = item.name;
+                        self.selected = item.roleName;
+                        self.roleList = data.roleList;
                     },
                     error: function () {
 
@@ -35,6 +46,42 @@ $(function () {
             },
             close:function(){
                 $(".box").hide();
+            },
+            save:function(){
+                var url = '';
+                var data = {};
+                if(isNew){
+                    url = _ctx+"/system/addUser";
+                    data = {
+
+                    }
+                }else{
+                    url = _ctx+"/system/editUser";
+                }
+                $.ajax({
+                    url: _ctx+"/system/editUser",
+                    method: "get",
+                    dataType: "json",
+                    data:{
+                        id: item.id
+                    },
+                    success: function (data) {
+                        self.userName = item.name;
+                        self.selected = item.roleName;
+                        self.roleList = data.roleList;
+                        self.pwd = "";
+                    },
+                    error: function () {
+
+                    }
+                });
+            },
+            initBox: function(){
+                self.userName = "";
+                self.selected = "";
+                self.roleList = [];
+                self.email = "";
+                self.pwd = "";
             }
         },
         created: function () {
