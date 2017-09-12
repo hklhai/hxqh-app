01416957a9f8fe5b2c2964828575d32a8b63bcf2
Anience <137heibaba>
2017-09-12 23:19:27 +0700

    update sla legend

--- src/main/webapp/js/ent/entUtil.js
+++ src/main/webapp/js/ent/entUtil.js
@@ -45,12 +45,28 @@ entUtil.prototype={
     },
     ShowSla: function (data,domName){
         var colorData1 = data;
-        var eq = colorData1.eq ==null?'0':colorData1.eq;
-        var gt = colorData1.gt ==null?'0':colorData1.gt;
-        var lt = colorData1.lt ==null?'0':colorData1.lt;
-        $(domName+" "+".span-layout .red").text(eq);
-        $(domName+" "+".span-layout .yellow").text(gt);
-        $(domName+" "+".span-layout .green").text(lt);
+        var eq = "";
+        var gt = "";
+        var lt = "";
+        if(domName.toString() == "Enterprise"){
+            eq = colorData1.eq ==null?'0':colorData1.eq + " ( >3.5H ) ";
+            gt = colorData1.gt ==null?'0':colorData1.gt + " ( =3.5H ) ";
+            lt = colorData1.lt ==null?'0':colorData1.lt + " (< 3.5H ) ";
+        }else if(domName.toString() == "Government"){
+            eq = colorData1.eq ==null?'0':colorData1.eq + " ( >3H ) ";
+            gt = colorData1.gt ==null?'0':colorData1.gt + " ( =3H ) ";
+            lt = colorData1.lt ==null?'0':colorData1.lt + " ( <3H ) ";
+        }else{
+            eq = colorData1.eq ==null?'0':colorData1.eq + " ( >4H ) ";
+            gt = colorData1.gt ==null?'0':colorData1.gt + " ( =4H ) ";
+            lt = colorData1.lt ==null?'0':colorData1.lt + " ( <4H ) ";
+        }
+        /*eq = colorData1.eq ==null?'0':colorData1.eq+"(>3)";
+        gt = colorData1.gt ==null?'0':colorData1.gt;
+        lt = colorData1.lt ==null?'0':colorData1.lt;*/
+        $(" "+".span-layout .red").text(eq);
+        $(" "+".span-layout .yellow").text(gt);
+        $(" "+".span-layout .green").text(lt);
     },
     showEvent: function(data,domName){
         var events = data;
